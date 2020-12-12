08bf189c81f38f7abe5d9d323dbfeb5f48b99ba3
Markus Opolka <markus@martialblog.de>
2020-12-12 20:26:29 +0100

    Add redis_exporter

---
+++ spec/classes/exporter_redis_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'hiera'
+
+describe 'vision_prometheus::exporter::redis' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+      context 'compile' do
+        it { is_expected.to compile.with_all_deps }
+      end
+    end
+  end
+end
